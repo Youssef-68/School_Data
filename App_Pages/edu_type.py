71d8eb7b6b4977966ed00769736d49be2784af32
Youssef Wael <y.wael1468@gmail.com>
2026-04-11 21:45:07 +0200

    Add import for Path to edu_type.py

--- App_Pages/edu_type.py
+++ App_Pages/edu_type.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pathlib import Path
 
 file_path = Path("education_cleaned.csv")
 df = pd.read_csv(file_path)
