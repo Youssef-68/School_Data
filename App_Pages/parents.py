29627ef32ebd7140deb7c70e32b04e8880a69523
Youssef Wael <y.wael1468@gmail.com>
2026-04-11 19:28:47 +0200

    Update parents.py

--- App_Pages/parents.py
+++ App_Pages/parents.py
@@ -2,10 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-file_path = os.path.join(BASE_DIR, "Data", "education_cleaned.csv")
-df = pd.read_csv(file_path)
+df = pd.read_csv("Data/education_cleaned.csv")
 st.title("Parents Education Analysis")
 
 # KPI Section
